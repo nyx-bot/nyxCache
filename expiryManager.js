ae657525c0aaa92ee220ea3943c674d23974ef54
Syl <syl@nyxbot.xyz>
2022-04-11 15:35:19 -0500

    don't cache every single db entry when looking for a small handful out of hundreds or thousands

--- expiryManager.js
+++ expiryManager.js
@@ -1,37 +1,45 @@
-const cron = require('cron');
 const fs = require('fs');
 
+const { Op } = require('sequelize')
+
 module.exports = () => {
-    const img = cron.job(`* * * * *`, /*runs every minute*/ async () => {
-        console.log(`Checking for pending file deletions...`)
-        
-        const allImages = await seq.models.Image.findAll();
+    setInterval(() => {
+        console.log(`Checking for pending file deletions...`);
 
-        const images = allImages.filter(img => {
-            const timeUntilDue = img.due - Date.now()
-            if(timeUntilDue <= 60000) return true
-            else return false;
-        });
-
-        const func = (i) => {
-            seq.models.Image.destroy({
-                where: {
-                    messageID: i.messageID,
-                    due: i.due,
-                    imageName: i.imageName
+        seq.models.Image.findAll({
+            where: {
+                due: {
+                    [Op.lte]: Date.now() + 60000
                 }
-            }).then(r => {
-                console.log(`Successfully removed db entry for ${i.messageID}-${i.imageName}`)
-                fs.rm(i.location, () => console.log(`Removed image ${i.messageID}-${i.imageName}`));
-            }).catch(e => {
-                console.error(`Unable to remove DB entry for ${i.messageID}-${i.imageName}`, e)
-            });
-        }
-        
-        for(i of images) {
-            const timeUntilDue = i.due - Date.now()
-            console.log(`> image ${i.messageID}-${i.imageName} is due in less than a minute (${timeUntilDue/1000}s)`);
-            if(timeUntilDue <= 0) {func(i)} else setTimeout(func, timeUntilDue, i)
-        }
-    }); img.start()
+            }
+        }).then(async images => {
+            const func = (i) => {
+                seq.models.Image.destroy({
+                    where: {
+                        messageID: i.messageID,
+                        due: i.due,
+                        imageName: i.imageName
+                    }
+                }).then(r => {
+                    console.log(`Successfully removed db entry for ${i.messageID}-${i.imageName}`)
+                    fs.rm(i.location, () => console.log(`Removed image ${i.messageID}-${i.imageName}`));
+                }).catch(e => {
+                    console.error(`Unable to remove DB entry for ${i.messageID}-${i.imageName}`, e);
+                    fs.rm(i.location, () => console.log(`Removed image ${i.messageID}-${i.imageName}`));
+                });
+            }
+            
+            for(i of images) {
+                const timeUntilDue = i.due - Date.now()
+                console.log(`> image ${i.messageID}-${i.imageName} is due in less than a minute (${timeUntilDue/1000}s)`);
+                if(timeUntilDue <= 0) {func(i)} else setTimeout(func, timeUntilDue, i)
+            }
+        })
+
+        //const images = allImages.filter(img => {
+        //    const timeUntilDue = img.due - Date.now()
+        //    if(timeUntilDue <= 60000) return true
+        //    else return false;
+        //});
+    }, 60000)
 }
